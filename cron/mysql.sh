f01e109d958d61334aaa521207614ba0cc8994d7
Tomasz Klim <github@tomaszklim.pl>
2016-11-21 13:51:52 +0100

    option in mysql backup to skip locking (for big production databases)

--- cron/mysql.sh
+++ cron/mysql.sh
@@ -8,5 +8,9 @@ DEST="$TMP/daily"
 if [ -f /etc/mysql/debian.cnf ] && [ -f /var/run/mysqld/mysqld.pid ]; then
 	pass="`cat /etc/mysql/debian.cnf |grep password |tail -n1 |sed s/password\ =\ //g`"
 	warn="Warning: Using a password on the command line interface can be insecure."
-	backup_mysql 127.0.0.1 3306 debian-sys-maint $pass mysql $TMP $DEST 2>&1 |grep -v "$warn"
+	if [ -f /etc/mysql/.nolock ]; then
+		backup_mysql 127.0.0.1 3306 debian-sys-maint $pass mysql $TMP $DEST skip 2>&1 |grep -v "$warn"
+	else
+		backup_mysql 127.0.0.1 3306 debian-sys-maint $pass mysql $TMP $DEST 2>&1 |grep -v "$warn"
+	fi
 fi
