abb93a0b53d134c5e2c5bd94707d7379dfb117c3
Tomasz Klim <github@tomaszklim.pl>
2017-03-12 11:59:38 +0100

    enabling backups of databases inside containers

--- setup.sh
+++ setup.sh
@@ -3,11 +3,6 @@
 . /opt/farm/scripts/functions.custom
 
 
-if [ "$HWTYPE" = "container" ] || [ "$HWTYPE" = "lxc" ]; then
-	echo "skipping system backup configuration"
-	exit 1
-fi
-
 if [ "`getent passwd backup`" = "" ]; then
 	echo "creating backup user and group"
 	if [ "$OSTYPE" = "freebsd" ]; then
@@ -26,6 +21,22 @@ mkdir -p            $path/daily $path/weekly $path/custom
 chmod 0700          $path/daily $path/weekly $path/custom
 chown backup:backup $path/daily $path/weekly $path/custom
 
+if ! grep -q /opt/farm/ext/backup/cron/mysql.sh /etc/crontab && [ -f /etc/mysql/debian.cnf ]; then
+	echo "setting up crontab entry for mysql backup"
+	echo "$((RANDOM%60)) 4 * * * root /opt/farm/ext/backup/cron/mysql.sh" >>/etc/crontab
+fi
+
+if ! grep -q /opt/farm/ext/backup/cron/postgres.sh /etc/crontab && [ -x /usr/bin/psql ]; then
+	echo "setting up crontab entry for postgres backup"
+	echo "$((RANDOM%60)) 4 * * * root /opt/farm/ext/backup/cron/postgres.sh" >>/etc/crontab
+fi
+
+
+if [ "$HWTYPE" = "container" ] || [ "$HWTYPE" = "lxc" ]; then
+	echo "skipping system backup configuration"
+	exit 1
+fi
+
 if [ "$OSTYPE" = "freebsd" ] || [ "$OSTYPE" = "netbsd" ]; then
 	mkdir -p   /var/lib
 	chmod 0711 /var/lib
@@ -36,24 +47,8 @@ if [ -d /boot ] && [ ! -h /boot ] && [ ! -f /boot/.done ]; then
 	touch /boot/.weekly /boot/.done
 fi
 
-if [ -h /etc/cron.daily/backup ]; then
-	echo "removing deprecated links to filesystem backup scripts"
-	rm -f /etc/cron.daily/backup
-	rm -f /etc/cron.weekly/backup
-fi
-
 if ! grep -q /opt/farm/ext/backup/cron/fs- /etc/crontab; then
 	echo "setting up filesystem backup scripts"
 	echo "$((RANDOM%60)) 6 * * * root /opt/farm/ext/backup/cron/fs-daily.sh" >>/etc/crontab
 	echo "$((RANDOM%60)) 6 * * 7 root /opt/farm/ext/backup/cron/fs-weekly.sh" >>/etc/crontab
-fi
-
-if ! grep -q /opt/farm/ext/backup/cron/mysql.sh /etc/crontab && [ -f /etc/mysql/debian.cnf ]; then
-	echo "setting up crontab entry for mysql backup"
-	echo "$((RANDOM%60)) 4 * * * root /opt/farm/ext/backup/cron/mysql.sh" >>/etc/crontab
-fi
-
-if ! grep -q /opt/farm/ext/backup/cron/postgres.sh /etc/crontab && [ -x /usr/bin/psql ]; then
-	echo "setting up crontab entry for postgres backup"
-	echo "$((RANDOM%60)) 4 * * * root /opt/farm/ext/backup/cron/postgres.sh" >>/etc/crontab
 fi
