322e3a07df786869fa07bb5c25f9b26b1538bee5
Tomasz Klim <github@tomaszklim.pl>
2017-11-08 16:57:50 +0100

    got rid of pg_list_local_databases function

--- cron/postgres.sh
+++ cron/postgres.sh
@@ -8,7 +8,7 @@ DEST="$TMP/daily"
 cd /tmp
 
 if [ -x /usr/bin/psql ] && [ -x /usr/bin/pg_dump ]; then
-	dbs=`pg_list_local_databases`
+	dbs=`sudo -u postgres psql -l -q 2>/dev/null |awk "{ print \\$1 }" |grep ^[a-zA-Z] |grep -v ^List$ |grep -v ^Name$ |grep -v ^template[0-9]$`
 	for db in $dbs; do
 		fname=`add_backup_extension postgres-$db.sql`
 		sudo -u postgres pg_dump -c $db |`stream_handler` >$TMP/$fname
