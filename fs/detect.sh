5373888d5c61e694713076ce6f80ac781e0e1505
Tomasz Klim <github@tomaszklim.pl>
2018-03-22 09:14:15 +0100

    detect qnap devices without relying on /opt/farm/scripts/init

--- fs/detect.sh
+++ fs/detect.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-. /opt/farm/scripts/init
 
-if [ "$OSTYPE" = "qnap" ]; then
+if [ -d /mnt/MD0_ROOT ] || [ -d /mnt/HDA_ROOT ]; then
 	/opt/farm/ext/backup/fs/dirs-qnap.sh
 else
 	/opt/farm/ext/backup/fs/dirs-linux.sh
