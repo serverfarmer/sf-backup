48fcbdd9bf956ab6636911b15993bb3185bff138
Tomasz Klim <github@tomaszklim.pl>
2019-05-10 16:40:25 +0200

    fixed docker volumes backup

--- cron/docker.sh
+++ cron/docker.sh
@@ -6,8 +6,12 @@
 TMP="`local_backup_directory`"
 DEST="$TMP/daily"
 
-for D in `/opt/farm/ext/backup/fs/docker.sh`; do
-	if [ -d $D ] && [ "`ls -A $D |grep -v hsperfdata_root`" != "" ] && [ ! -f $D/.nobackup ]; then
-		backup_directory $TMP $DEST $D docker-$VOL.tar
-	fi
-done
+if [ -x /usr/bin/docker ]; then
+	for VOL in `/usr/bin/docker volume list |grep ^local |awk '{ print $2 }'`; do
+		D=`/usr/bin/docker inspect $VOL |grep '"Mountpoint"' |awk '{ print $2 }' |sed -e s/\"//g -e s/,//g`
+
+		if [ -d $D ] && [ "`ls -A $D |grep -v hsperfdata_root`" != "" ] && [ ! -f $D/.nobackup ]; then
+			backup_directory $TMP $DEST $D docker-$VOL.tar
+		fi
+	done
+fi
