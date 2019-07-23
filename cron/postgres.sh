558e3b6237e139f0e41cafdbde470aa7d575c170
Tomasz Klim <github@tomaszklim.pl>
2019-07-23 10:30:22 +0200

    hide warnings from which

--- cron/postgres.sh
+++ cron/postgres.sh
@@ -8,7 +8,7 @@ DEST="$TMP/daily"
 
 cd /tmp
 
-if [ "`which psql`" != "" ] && [ "`which pg_dump`" != "" ] && [ "`getent passwd postgres`" != "" ]; then
+if [ "`which psql 2>/dev/null`" != "" ] && [ "`which pg_dump 2>/dev/null`" != "" ] && [ "`getent passwd postgres`" != "" ]; then
 	dbs=`sudo -u postgres psql -l -q -X 2>/dev/null |awk "{ print \\$1 }" |grep ^[a-zA-Z] |grep -v ^List$ |grep -v ^Name$ |grep -v ^template[0-9]$`
 	for db in $dbs; do
 		fname=`add_backup_extension postgres-$db.sql`
